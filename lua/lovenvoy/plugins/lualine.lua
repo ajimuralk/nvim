e9033d4eefff8f5d4f1d7e1ed6274be82e309396
Landon Ajimura <lajimura@enbala.com>
2023-02-07 09:38:08 -0800

    Move gitblame config to fix missing filetype in lualine

--- lua/lovenvoy/plugins/lualine.lua
+++ lua/lovenvoy/plugins/lualine.lua
@@ -12,26 +12,22 @@ local new_colors = {
 	black = "#000000",
 }
 
+vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+local git_blame = require("gitblame")
+
 -- configure lualine with modified theme
 lualine.setup({
 	options = {
 		theme = "nord",
 	},
 	sections = {
-		lualine_c = { {
-			"filename",
-			file_status = true,
-			path = 3,
-		} },
-	},
-})
-
-vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
-local git_blame = require("gitblame")
-
-require("lualine").setup({
-	sections = {
 		lualine_c = {
+			{
+				"filename",
+				file_status = true,
+				path = 3,
+			},
+
 			{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
 		},
 	},
