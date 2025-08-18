8f1aa3bf8dff1788c9fe064c8199eff2c07d15ed
Landon Ajimura <landon.ajimura@hiive.com>
2025-08-18 16:18:27 -0700

    Lualine updates

--- lua/lovenvoy/plugins/lualine.lua
+++ lua/lovenvoy/plugins/lualine.lua
@@ -15,19 +15,27 @@ local new_colors = {
 vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
 local git_blame = require("gitblame")
 
+local function short_branch()
+  local branch = vim.b.gitsigns_head or ''
+  if #branch > 15 then
+    return branch:sub(1, 20) .. '..'
+  end
+  return branch
+end
+
 -- configure lualine with modified theme
 lualine.setup({
 	options = {
 		theme = "nord",
 	},
 	sections = {
+		lualine_b = { short_branch, "diff", "diagnostics" },
 		lualine_c = {
-			{
-				"filename",
-				file_status = true,
-				path = 3,
-			},
-
+			-- {
+			-- 	"filename",
+			-- 	file_status = true,
+			-- 	path = 3,
+			-- },
 			{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
 		},
 	},
